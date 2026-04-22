5d1639136e62eb26d20ef35d23b989acb1a3cc8c
MD. Nur Hasan Nishad <nishad@jrfintel.com>
2026-04-22 10:08:38 +0600

    first commit

--- public/index.php
+++ public/index.php
@@ -5,11 +5,16 @@ use Illuminate\Http\Request;
 define('LARAVEL_START', microtime(true));
 
 // Maintenance
+<<<<<<< HEAD
 if (file_exists($maintenance = __DIR__.'/quran_api/storage/framework/maintenance.php')) {
+=======
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+>>>>>>> fe43a2e (first commit)
     require $maintenance;
 }
 
 // Autoload
+<<<<<<< HEAD
 require __DIR__.'/quran_api/vendor/autoload.php';
 
 // Bootstrap
@@ -17,3 +22,18 @@ $app = require_once __DIR__.'/quran_api/bootstrap/app.php';
 
 // Run
 $app->handleRequest(Request::capture());
+=======
+require __DIR__.'/../vendor/autoload.php';
+
+// Bootstrap
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+// Run Laravel properly
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Request::capture()
+)->send();
+
+$kernel->terminate($request, $response);
+>>>>>>> fe43a2e (first commit)
