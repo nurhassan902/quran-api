873640648cb067e10a966cdf7ac2a3db0855dcb6
MD. Nur Hasan Nishad <nishad@jrfintel.com>
2026-04-22 10:28:56 +0600

    fix PDO issue

--- public/index.php
+++ public/index.php
@@ -5,24 +5,11 @@ use Illuminate\Http\Request;
 define('LARAVEL_START', microtime(true));
 
 // Maintenance
-<<<<<<< HEAD
-if (file_exists($maintenance = __DIR__.'/quran_api/storage/framework/maintenance.php')) {
-=======
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
->>>>>>> fe43a2e (first commit)
     require $maintenance;
 }
 
 // Autoload
-<<<<<<< HEAD
-require __DIR__.'/quran_api/vendor/autoload.php';
-
-// Bootstrap
-$app = require_once __DIR__.'/quran_api/bootstrap/app.php';
-
-// Run
-$app->handleRequest(Request::capture());
-=======
 require __DIR__.'/../vendor/autoload.php';
 
 // Bootstrap
@@ -36,4 +23,3 @@ $response = $kernel->handle(
 )->send();
 
 $kernel->terminate($request, $response);
->>>>>>> fe43a2e (first commit)
